c954f0037281246639a104beccc5cfd722ec3128
youngmoo99 <rladuan612@gmail.com>
2024-12-21 12:50:20 +0900

    player mouse move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,10 +8,33 @@ public class Player : MonoBehaviour
     private float moveSpeed;
     // Update is called once per frame
     void Update()
-    {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
-        Vector3 moveTo = new Vector3(horizontalInput, verticalInput, 0f);
-        transform.position += moveTo * moveSpeed * Time.deltaTime;
+    {   
+        //키보드 제어
+        //float horizontalInput = Input.GetAxisRaw("Horizontal");
+        //float verticalInput = Input.GetAxisRaw("Vertical");
+        // Vector3 moveTo = new Vector3(horizontalInput, 0f, 0f);
+        // transform.position += moveTo * moveSpeed * Time.deltaTime;
+
+        // Vector3 moveTo = new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        // if (Input.GetKey(KeyCode.LeftArrow)) {  //왼쪽 방향키일때
+        //     transform.position -= moveTo;
+        // } else if(Input.GetKey(KeyCode.RightArrow)) { //오른쪽 방향키일때
+        //     transform.position += moveTo;
+        // }
+
+
+        //마우스 제어 
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.Log(mousePos); 로그남기기 
+        
+        //mathf.clamp(값, 최소값, 최대값) --> 최소,최대값을 벗어난경우 해당수치로 변경, 그 사이값일 경우 값 그대로 
+        float toX = Mathf.Clamp(mousePos.x,-2.35f,2.35f);
+        //x값만 변경된 포지션 y z 값은 원래값 그대로 
+        transform.position = new Vector3(toX, transform.position.y, transform.position.z);
+
+
+
+
+
     }
 }
