e04b073c2ac3a28936ecd69b9a7f1e8c54528770
youngmoo99 <rladuan612@gmail.com>
2024-12-27 17:12:46 +0900

    create unlimited enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,11 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 10f;
     private float minY = -7;
+
+    public void SetMoveSpeed(float moveSpeed) {
+        this.moveSpeed = moveSpeed; // this를 붙일경우 클래스내에 있는 변수 
+        
+    }
     // Update is called once per frame
     void Update()
     {
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,19 +8,56 @@ public class EnemySpawner : MonoBehaviour
     private GameObject[] enemies;
 
     private float[] arrPosX = { -2.2f, -1.1f, 0, 1.1f, 2.2f };
+    [SerializeField]
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {   
-        foreach (float posX in arrPosX) {
-            //적 개수 랜덤 0~6
-            int index = Random.Range(0,enemies.Length);
-            SpawnEnemy(posX, index);
+        StartEnemyRoutine();
+    }
+
+    void StartEnemyRoutine() {
+        StartCoroutine("EnemyRoutine");
+    }
+
+    IEnumerator EnemyRoutine() {
+        //3초 지난후에 밑에 반복 실행
+        yield return new WaitForSeconds(3f);     
+
+        float moveSpeed = 5f;
+        int enemyIndex = 0;
+        int spawnCount = 0;
+        while (true) {
+            foreach (float posX in arrPosX) {
+                //적 개수 랜덤 0~6
+                //int index = Random.Range(0,enemies.Length);
+                SpawnEnemy(posX, enemyIndex, moveSpeed);
+            }
+            spawnCount++;
+            if (spawnCount % 10 == 0) {
+                enemyIndex++;
+                moveSpeed+=2;
+            }
+
+            yield return new WaitForSeconds(spawnInterval);   
         }
     }
 
     //적 5개 생성 
-    void SpawnEnemy(float posX, int index) {
+    void SpawnEnemy(float posX, int index, float moveSpeed) {
         Vector3 spawnPos = new Vector3(posX,transform.position.y,transform.position.z);
-        Instantiate(enemies[index], spawnPos, Quaternion.identity);
+
+        //0~4 랜덤숫자 
+        if(Random.Range(0,5) == 0) {
+            index +=1;
+        }
+        if(index >= enemies.Length) {
+            index = enemies.Length-1;
+        }
+
+        //Enemy 클래스에 setmovespeed 적용 (시간이 지날수록 쓰레기 속도 증가)
+        GameObject enemyObject = Instantiate(enemies[index], spawnPos, Quaternion.identity);
+        Enemy enemy = enemyObject.GetComponent<Enemy>();
+        enemy.SetMoveSpeed(moveSpeed);
     }
 }
